d22292f8507c5ad5649783b23d89b3f060bf92a7
Mohammed-Aymen Benadra <aymanbenadra16@gmail.com>
2022-02-25 10:21:20 +0100

    Deploy to Heroku

--- app/views/posts/show.view.php
+++ app/views/posts/show.view.php
@@ -12,28 +12,30 @@
 
     ?>
     <section class="post">
-        <img class="post__photo" src="<?= "assets/uploads/".$post->photo ?>" alt="<?= $post->title ?>">
+        <img class="post__photo" src="<?= "assets/uploads/" . $post->photo ?>" alt="<?= $post->title ?>">
         <div class="post__content">
             <h1 class="post__title"><?= $post->title ?></h1>
-            <h3 class="post__category"><?= $post->category_id == 1 ? "Movie" : "Serie" ;?></h3>
+            <h3 class="post__category"><?= $post->category_id == 1 ? "Movie" : "Serie"; ?></h3>
             <p class="post__description"><?= $post->description ?></p>
         </div>
     </section>
     <?php
+    if (isset($_SESSION['user'])) :
         if ($user->id == $post->author_id) :
     ?>
-    <div class="post__actions">
-        <ul class="post__actions-list">
-            <a href="/post/edit?id=<?= $post->id ?>" class="post__actions-item">
-                <button class="btn btn-success" type="submit">Edit</button>
-            </a>
-            <form action="/post/destroy" method="post" class="post__actions-item">
-                <input type="hidden" name="id" value="<?= $post->id ?>">
-                <button class="btn btn-danger" type="submit">Delete</button>
-            </form>
-        </ul>
-    </div>
-    <?php endif; ?>
+            <div class="post__actions">
+                <ul class="post__actions-list">
+                    <a href="/post/edit?id=<?= $post->id ?>" class="post__actions-item">
+                        <button class="btn btn-success" type="submit">Edit</button>
+                    </a>
+                    <form action="/post/destroy" method="post" class="post__actions-item">
+                        <input type="hidden" name="id" value="<?= $post->id ?>">
+                        <button class="btn btn-danger" type="submit">Delete</button>
+                    </form>
+                </ul>
+            </div>
+    <?php endif;
+    endif; ?>
     <section class="comments">
         <div class="card">
             <div class="card-header text-center">
@@ -50,15 +52,17 @@
                             <div class="comment">
                                 <p class="comment__content"><?= $comment->content ?><br><span class="comment__time">~ <?= $now ?></span></p>
                                 <?php
+                                if (isset($_SESSION['user'])) :
                                     if ($user->id == $comment->author_id) :
                                 ?>
-                                <form action="/comment/destroy" method="post">
-                                    <input type="hidden" name="id" value="<?= $comment->id ?>">
-                                    <input type="hidden" name="post_id" value="<?= $post->id ?>">
-                                    <button type="submit" class="btn btn-danger btn-sm">Delete</button>
-                                </form>
+                                        <form action="/comment/destroy" method="post">
+                                            <input type="hidden" name="id" value="<?= $comment->id ?>">
+                                            <input type="hidden" name="post_id" value="<?= $post->id ?>">
+                                            <button type="submit" class="btn btn-danger btn-sm">Delete</button>
+                                        </form>
                                 <?php
                                     endif;
+                                endif;
                                 ?>
                             </div>
                         </li>
@@ -72,16 +76,19 @@
                         </div>
                     </li>
                 <?php endif; ?>
-                <li class="list-group-item">
-                    <form action="/comment/store" method="post">
-                        <div class="input-group">
-                            <textarea class="form-control" aria-label="content" name="content" rows="1" cols="100"></textarea>
-                            <input type="hidden" name="post_id" value="<?= $post->id ?>">
-                            <input type="hidden" name="author_id" value="<?= $user->id ?>">
-                            <button class="btn btn-outline-secondary" type="submit">Submit</button>
-                        </div>
-                    </form>
-                </li>
+
+                <?php if (isset($_SESSION['user'])) : ?>
+                    <li class="list-group-item">
+                        <form action="/comment/store" method="post">
+                            <div class="input-group">
+                                <textarea class="form-control" aria-label="content" name="content" rows="1" cols="100"></textarea>
+                                <input type="hidden" name="post_id" value="<?= $post->id ?>">
+                                <input type="hidden" name="author_id" value="<?= $user->id ?>">
+                                <button class="btn btn-outline-secondary" type="submit">Submit</button>
+                            </div>
+                        </form>
+                    </li>
+                <?php endif; ?>
             </ul>
         </div>
     </section>
