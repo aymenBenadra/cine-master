511f05b8b5582f2cdff6b900ff941b3c388aeac0
Mohammed-Aymen Benadra <aymanbenadra16@gmail.com>
2022-04-13 15:19:13 +0000

    Change avatar function added

--- public/js/script.js
+++ public/js/script.js
@@ -3,3 +3,14 @@ function preview() {
     preview.style.display = 'block';
     frame.src = URL.createObjectURL(event.target.files[0]);
 }
+
+// A function to replace the image in the avatar input field with the image in the preview if the user chooses to change it
+function changeAvatar() {
+    let avatar = document.getElementById('avatar');
+    let preview = document.getElementById('preview');
+    // replace avatar src with the src file name of the preview image
+    avatar.src = URL.createObjectURL(event.target.files[0]);
+    alert('Avatar changed!', avatar.src);
+
+    return false;
+}
